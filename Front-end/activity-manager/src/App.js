b55d8cffe13705a0ece4899b7f9c53ad534ad308
João Correia <joao.correia@ivoryit.com.br>
2022-04-15 17:57:54 -0300

    Limpando projeto e adicionando bootstrap

--- Front-end/activity-manager/src/App.js
+++ Front-end/activity-manager/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <ul className="list-group">
+        <li className="list-group-item">Teste 1</li>
+        <li className="list-group-item">Teste 2</li>
+        <li className="list-group-item">Teste 3</li>
+      </ul>
     </div>
   );
 }
